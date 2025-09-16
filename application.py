805373b421e6be7de2c43c969b5e104d0da0766a
Prasana <bprasana85@gmail.com>
2025-09-16 20:50:26 +0100

    Update application.py

--- application.py
+++ application.py
@@ -36,5 +36,6 @@ def predict_datapoint():
     else:
         return render_template('home.html')
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # EB expects 8080
+    app.run(host="0.0.0.0", port=port)
